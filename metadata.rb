b6938d1012cd8d5c36f38dc30bea6cbb3ffed92c
Jim Rosser <jarosser06@gmail.com>
2014-01-27 09:32:26 -0600

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'kibana'
+maintainer       'Texas A&M'
+maintainer_email 'jarosser06@tamu.edu'
+license          'MIT'
+description      'Installs/Configures kibana'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'apache2'
+depends          'nginx'
